ee4ac98cecc0d5e75a0e5131fa5b2d73c2055f6e
Matthieu De Laere <Matthieu De Laere>
2016-03-03 19:56:21 +0100

    Removing old shit.

--- src/projects/Role.java
+++
@@ -1,7 +0,0 @@
-package projects;
-
-public enum Role {
-	LEAD, 			// This person is the lead developer of the project
-	PROGRAMMER,		// This person is a programmer on the project
-	TESTER			// This person is a tester for the project
-}
