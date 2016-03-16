c2243368d601ed2929a3bb5ccc52e27fc10d6978
Mathijs Delabie <Mathijs@Mathijss-MacBook-Pro.local>
2016-03-16 15:01:25 +0100

    edited tests

--- src/tests/LoginUseCaseTest.java
+++ src/tests/LoginUseCaseTest.java
@@ -1,38 +1,36 @@
 package tests;
 
-import controllers.UserController;
-import model.BugTrap;
-import model.users.User;
-import model.users.UserCategory;
-import model.users.UserManager;
-import model.users.exceptions.NoUserWithUserNameException;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
+import model.users.User;
+import model.users.UserCategory;
+import model.users.UserManager;
+import model.users.exceptions.NoUserWithUserNameException;
 
 
 public class LoginUseCaseTest {
 	
-	UserController controller;
+	UserManager userManager;
 
 	@Before
 	public void setUp() throws Exception {
-		controller = new UserController(new BugTrap());
 		//add user
-		UserManager userMan = (UserManager) controller.getBugTrap().getUserManager();
-		userMan.createUser(UserCategory.ADMIN, "", "", "", "ADMIN");
+		userManager = new UserManager();
+		userManager.createUser(UserCategory.ADMIN, "", "", "", "ADMIN");
 	}
 
 	@Test
 	public void loginSuccesTest() {
 		//step 1
-		List<User> list = controller.getUserList(UserCategory.ADMIN);
+		List<User> list = userManager.getUserList(UserCategory.ADMIN);
 		//step 2
 		User user = list.get(0);
 		//step 3
-		String message = controller.loginAs(user);
+		String message = userManager.loginAs(user);
 		//step 4
 		Assert.assertEquals("User: ADMIN successfully logged in.",message);
 	}
@@ -40,7 +38,7 @@ public class LoginUseCaseTest {
 	@Test (expected = NoUserWithUserNameException.class)
 	public void loginNoUserTest() {
 		//step 3, wrong input
-		controller.loginAs(null);
+		userManager.loginAs(null);
 	}
 
 }
