858d0a126811040cbe885d74ec7ff1f811a73178
Mathijs Delabie <Mathijs@Mathijss-MacBook-Pro.local>
2016-03-18 17:19:42 +0100

    assign project tests (not finished) needs certain implementations

--- src/tests/AssignProjectUseCaseTest.java
+++ src/tests/AssignProjectUseCaseTest.java
@@ -1,7 +1,10 @@
 package tests;
 
 
+import static org.junit.Assert.*;
+
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -17,62 +20,129 @@ import model.projects.Role;
 import model.projects.Version;
 import model.projects.forms.ProjectAssignForm;
 import model.users.Developer;
+import model.users.User;
 
 public class AssignProjectUseCaseTest {
 
 	private BugTrap bugTrap;
-	private ProjectManager projectManager;
-	private FormFactory formFactory;
 
 	@Before
 	public void setUp() throws Exception {
 		bugTrap = new BugTrap();
-		projectManager = bugTrap.getProjectManager();
-		formFactory = new FormFactory(bugTrap);
-
-		bugTrap.getUserManager().createAdmin();
+		
+		//add user
+		Developer dev = bugTrap.getUserManager().createDeveloper("", "", "", "DEV");
+		bugTrap.getUserManager().createDeveloper("", "", "", "DEV2");
+		bugTrap.getUserManager().createAdmin("", "", "", "ADMIN");
+		
+		ProjectTeam team = new ProjectTeam();
+		team.addMember(dev, Role.LEAD);
+		bugTrap.getProjectManager().createProject("name", "description", new Date(1302), new Date(1302), 1234, team, new Version(1, 0, 0));
+		
 	}
 
 	@Test
-	public void assignProgrammerToProjectTest() throws UnauthorizedAccessException {
-		ProjectAssignForm form = formFactory.makeProjectAssignForm();
-		Project project = projectManager.createProject("", "", new Date(), new Date(), 123, new ProjectTeam(), new Version(1, 0, 0));
-		Developer dev = new Developer("", "", "", "");
-
-		form.setRole(Role.PROGRAMMER);
-		form.setDeveloper(dev);
+	public void assignProgrammerToProjectTest(){
+		//login
+		User dev = bugTrap.getUserManager().getUser("DEV");
+		bugTrap.getUserManager().loginAs(dev);
+				
+		
+		//step 1
+		ProjectAssignForm form = null;
+		try {
+			form = bugTrap.getFormFactory().makeProjectAssignForm();
+		} catch (UnauthorizedAccessException e) {
+			fail("not authorized");
+			e.printStackTrace();
+		}
+		//step 2 TODO get logged in user (is dev?)
+		List<Project> projects = bugTrap.getProjectManager().getProjectsForLeadDeveloper((Developer) dev);
+		//step 3
+		Project project =  projects.get(0);
 		form.setProject(project);
+		//step 4
+		List<Developer> devs = bugTrap.getUserManager().getDevelopers();
+		//step 5
+		Developer developer = devs.get(1);
+		form.setDeveloper(developer);
+		//step 6 TODO get roles not yet assigned to developer?
+		//bugTrap.getProjectManager().getRoles();
+		//step 7
+		form.setRole(Role.PROGRAMMER);
 
-		projectManager.assignToProject(form);
+		bugTrap.getProjectManager().assignToProject(form);
 
-		Assert.assertEquals(project.getTeam().getProgrammers().get(0), dev);
+		Assert.assertEquals(project.getTeam().getProgrammers().get(0), developer);
 	}
 
 	@Test
 	public void assignLeadToProjectTest() throws UnauthorizedAccessException {
-		ProjectAssignForm form = formFactory.makeProjectAssignForm();
-		Project project = projectManager.createProject("", "", new Date(), new Date(), 123, new ProjectTeam(), new Version(1, 0, 0));
-		Developer dev = new Developer("", "", "", "");
+		//login
+				User dev = bugTrap.getUserManager().getUser("DEV");
+				bugTrap.getUserManager().loginAs(dev);
+						
+				
+				//step 1
+				ProjectAssignForm form = null;
+				try {
+					form = bugTrap.getFormFactory().makeProjectAssignForm();
+				} catch (UnauthorizedAccessException e) {
+					fail("not authorized");
+					e.printStackTrace();
+				}
+				//step 2 TODO get logged in user (is dev?)
+				List<Project> projects = bugTrap.getProjectManager().getProjectsForLeadDeveloper((Developer) dev);
+				//step 3
+				Project project =  projects.get(0);
+				form.setProject(project);
+				//step 4
+				List<Developer> devs = bugTrap.getUserManager().getDevelopers();
+				//step 5
+				Developer developer = devs.get(1);
+				form.setDeveloper(developer);
+				//step 6 TODO get roles not yet assigned to developer?
+				//bugTrap.getProjectManager().getRoles();
+				//step 7
+				form.setRole(Role.LEAD);
 
-		form.setRole(Role.LEAD);
-		form.setDeveloper(dev);
-		form.setProject(project);
+				bugTrap.getProjectManager().assignToProject(form);
 
-		Assert.assertEquals(project.getTeam().getLeadDeveloper(), dev);
-	}
+				Assert.assertEquals(project.getTeam().getLeadDeveloper(), developer);
+			}
 
 	@Test
 	public void assignTesterToProjectTest() throws UnauthorizedAccessException {
-		ProjectAssignForm form = formFactory.makeProjectAssignForm();
-		Project project = projectManager.createProject("", "", new Date(), new Date(), 123, new ProjectTeam(), new Version(1, 0, 0));
-		Developer dev = new Developer("", "", "", "");
+		//login
+				User dev = bugTrap.getUserManager().getUser("DEV");
+				bugTrap.getUserManager().loginAs(dev);
+						
+				
+				//step 1
+				ProjectAssignForm form = null;
+				try {
+					form = bugTrap.getFormFactory().makeProjectAssignForm();
+				} catch (UnauthorizedAccessException e) {
+					fail("not authorized");
+					e.printStackTrace();
+				}
+				//step 2 TODO get logged in user (is dev?)
+				List<Project> projects = bugTrap.getProjectManager().getProjectsForLeadDeveloper((Developer) dev);
+				//step 3
+				Project project =  projects.get(0);
+				form.setProject(project);
+				//step 4
+				List<Developer> devs = bugTrap.getUserManager().getDevelopers();
+				//step 5
+				Developer developer = devs.get(1);
+				form.setDeveloper(developer);
+				//step 6 TODO get roles not yet assigned to developer?
+				//bugTrap.getProjectManager().getRoles();
+				//step 7
+				form.setRole(Role.TESTER);
 
-		form.setRole(Role.TESTER);
-		form.setDeveloper(dev);
-		form.setProject(project);
+				bugTrap.getProjectManager().assignToProject(form);
 
-		projectManager.assignToProject(form);
-
-		Assert.assertEquals(project.getTeam().getTesters().get(0), dev);
-	}
+				Assert.assertEquals(project.getTeam().getTesters().get(0), developer);
+			}
 }
