3390317f8faac8be9295c8d646e338c54849a8f1
Matthieu De Laere <Matthieu De Laere>
2016-03-18 10:49:41 +0100

    BugTag, UnauthorizedAccess, Controller doczzz

--- src/model/bugreports/bugtag/Assigned.java
+++ src/model/bugreports/bugtag/Assigned.java
@@ -1,5 +1,10 @@
 package model.bugreports.bugtag;
 
+/**
+ * This class represents the Assigned bug tag.
+ * This means that a BugReport has been assigned to a
+ * developer to be resolved.
+ */
 public class Assigned extends BugTag {
 	
 	public Assigned() {
