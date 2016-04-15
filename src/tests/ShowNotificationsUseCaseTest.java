67530db3a1eb537b641f3a09f560b52d2fd8c0c2
Niels Elgers <Niels Elgers>
2016-04-15 15:49:26 +0200

    bleh

--- src/tests/ShowNotificationsUseCaseTest.java
+++ src/tests/ShowNotificationsUseCaseTest.java
@@ -20,7 +20,6 @@ import model.bugreports.bugtag.BugTag;
 import model.notifications.INotification;
 import model.notifications.RegistrationType;
 import model.notifications.forms.ShowChronologicalNotificationForm;
-import model.notifications.observers.BugReportChangeObserver;
 import model.projects.Project;
 import model.projects.Version;
 
