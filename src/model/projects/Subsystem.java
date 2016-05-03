5c98db7d562882b8c22c4e928f867a9fdc8b02a6
Mathijs Delabie <Mathijs@Mathijss-MacBook-Pro.local>
2016-05-03 15:47:32 +0200

    Split and merge subsystem is NOW possible! (kind of)

--- src/model/projects/Subsystem.java
+++ src/model/projects/Subsystem.java
@@ -2,6 +2,8 @@ package model.projects;
 
 import java.util.List;
 
+import model.bugreports.IBugReport;
+
 /**
  * This class represents a subsystem in a system.
  */
@@ -46,5 +48,84 @@ public class Subsystem extends System implements ISubsystem {
 	public void terminate() {
 		super.terminate();
 		project = null;
+	}
+	
+	/**
+	 * Splits a subsystem into two new subsystems with given names and descriptions.
+	 * The first new subsystem receives the given bug reports and subsystems.
+	 * The second new subsystem receives the remaining bug reports and subsystems.
+	 * @param nameFor1 The name for the first new subsystem
+	 * @param nameFor2 The name for the second new subsystem
+	 * @param descriptionFor1 The description for the first new subsystem
+	 * @param descriptionFor2 The description for the second new subsystem
+	 * @param bugReportsFor1 The bug reports for the first new subsystem
+	 * @param subsystemsFor1 The subsystems for the first new subsystem
+	 */
+	public void splitSubsystem(String nameFor1, String nameFor2, String descriptionFor1, String descriptionFor2,
+			List<IBugReport> bugReportsFor1, List<ISubsystem> subsystemsFor1){
+		Subsystem sub1 = new Subsystem(nameFor1, descriptionFor1, parent, null, project, getAchievedMilestone());
+		Subsystem sub2 = new Subsystem(nameFor2, descriptionFor2, parent, null, project, getAchievedMilestone());
+		
+		for (Subsystem subsystem : this.subsystems) {	
+			if(subsystemsFor1.contains(subsystem)){
+				//subsystem for first new subsystem
+				sub1.subsystems.add(subsystem); 
+				this.subsystems.remove(subsystem);
+			}else{
+				//subsystem for second new subsystem
+				sub2.subsystems.add(subsystem); 
+				this.subsystems.remove(subsystem);	
+			}
+		}
+		
+		parent.subsystems.add(sub1);
+		parent.subsystems.add(sub2);
+		
+		terminate();
+		parent.subsystems.remove(this);
+	}
+	
+	/**
+	 * Merges this subsystem with a given subsystem that is a child, parent or sibling of this subsystem.
+	 * The new merged subsystem gets a given name and description.
+	 * @param name
+	 * @param description
+	 * @param subsystem
+	 */
+	public void mergeSubsystem(String name, String description, ISubsystem iSubsystem){
+		//if parent and child merge, the parent has the responsibility
+		if(this.getParent().equals(iSubsystem)){
+			((Subsystem) iSubsystem).mergeSubsystem(name, description, this);
+			return;
+		}
+		
+		AchievedMilestone achievedMilestone = this.getAchievedMilestone();
+		if(iSubsystem.getAchievedMilestone().compareTo(achievedMilestone) < 0){
+			achievedMilestone = iSubsystem.getAchievedMilestone();
+		}
+		
+		Subsystem newSubsystem = new Subsystem(name, description, parent, null, project, achievedMilestone);
+		
+		//move subsystems from given subsystem (=sibling or child)
+		Subsystem subsystem = ((Subsystem) iSubsystem);
+		for (Subsystem sub : subsystem.subsystems) {
+			newSubsystem.subsystems.add(sub);
+			subsystem.subsystems.remove(sub);
+		}
+		//move subsystems from this subsystem (=sibling or parent)
+		for (Subsystem sub : this.subsystems) {
+			if(!sub.equals(subsystem)){
+				newSubsystem.subsystems.add(sub);
+				subsystem.subsystems.remove(sub);
+			}
+		}
+		
+		//delete given subsystem (=child or sibling)
+		System parent = subsystem.parent;
+		subsystem.terminate();
+		parent.subsystems.remove(subsystem);
+		//delete this subsystem (=parent or sibling)
+		terminate();
+		this.parent.subsystems.remove(this);
 	}
 }
